cfe7893a0ef0f057acf56952140b3ead74a2f110
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-09-08 14:56:47 -0300

    #005: Constants

---
+++ 005/ConstantsExample.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract ConstantsExample {
+
+    // Constants ensure that a variable's value remains unchanged once assigned.
+    // Benefits: Immutability, Gas Optimization, Code Readability, Reusability
+
+    address public constant MY_ADDRESS = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    uint public constant MY_UINT = 123;
+
+    function getMyAddress() public view returns (address) {
+        return MY_ADDRESS;
+    }
+
+    function getMyUint() public view returns (MY_UINT) {
+        return MY_UINT;
+    }
+
+}
