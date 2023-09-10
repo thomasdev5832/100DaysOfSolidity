915ae69eca1ba80a5504b4d77babf4ddfc66c0d3
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-09-10 10:44:19 -0300

    #007 - State Variable (Reading & Writing)

---
+++ 007 - State Variable (Reading and Writing)/SimpleStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract SimpleStorage {
+
+    uint public num;
+
+    function set(uint _num) public {
+        num = _num;
+    }
+
+    function get() public view returns (uint) {
+        return num;
+    }
+
+}
