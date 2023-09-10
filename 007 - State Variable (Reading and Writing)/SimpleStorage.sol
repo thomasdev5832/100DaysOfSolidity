20078baeaec32fa4826f28d623cee60dd35629d7
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-09-10 10:51:09 -0300

    #007 - State Variable (Reading & Writing)

--- 007 - State Variable (Reading and Writing)/SimpleStorage.sol
+++ 007 - State Variable (Reading and Writing)/SimpleStorage.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.17;
 
 contract SimpleStorage {
 
+    // State variable to store a number
     uint public num;
 
+    // need to send a transaction to write to a state variable.
     function set(uint _num) public {
         num = _num;
     }
 
+    // read from a state variable without sending a transaction.
+    // retrieve the current value of `num` without incurring any transaction costs
     function get() public view returns (uint) {
         return num;
     }
