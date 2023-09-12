3f87057ffc47ea6d2c122d286f85453160b1a2b9
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-09-12 10:21:29 -0300

    #009 - Gas & Ether: gasleft, gas price, gas fees

--- 009 - Gas and Ether/GasFees.sol
+++ 009 - Gas and Ether/GasFees.sol
@@ -8,9 +8,11 @@ contract GasFees {
 
     function transfer(uint _value) external payable {
         
-        // calculate gas fees
+        // Gasleft is used to check the remaining gas during a contract call
         uint gasUsed = gasleft();
+        // Gas price represents the amount of ether you are willing to pay per unit of gas.
         uint gasPrice = tx.gasprice;
+        // calculate gas fees
         uint gasFees = gasUsed * gasPrice;
 
         // deduct gas fees from the transfered value
