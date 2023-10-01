bacdf6b5c4924a4d3def20cf49499d690e88ae7f
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-10-01 11:45:17 -0300

    #028 - Payable

---
+++ 028 - Payable/AnotherPayable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+contract AnotherPayable {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {}
+
+    function notPayable() public {}
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        uint amount = address(this).balance;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function transfer(address _to, uint _amount) public {
+        require(msg.sender == owner, "Only owner can transfer");
+        payable(_to).transfer(_amount);
+    }
+
+}
---
+++ 028 - Payable/Payable.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+contract Payable {
+    
+    // payable address can receive Ether
+    address payable public owner;
+
+    // payable constructor can receive Ether
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    // The `deposit` function allows anyone to send Ether to the contract.
+    // The `payable` keyword ensures that the function can receive Ether. 
+    function deposit() public payable {
+        
+    }
+
+    // Thos function is not payable, cannot receive Ether
+    // If someone tries to send Ether to this function, it will throw an error.
+    function notPayable() public {
+        
+    }
+
+    // function to withdraw all Ether from this contract
+    function withdraw() public {
+        
+        // get the amount of Ether stored in this contract
+        uint amount = address(this).balance;
+
+        // send all Ether to owner
+        // owner can receive Ether since the address of owner is payable
+        (bool success, ) = owner.call{value: amount}("");
+
+        require(success, "Failed to send Ether");
+    }
+
+    // Function to transfer Ether from this contract to address from input
+    function transfer(address payable _to, uint _amount) public {
+        
+        // note that "to" is declared as payable
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Failed to send Etehr");
+    }
+
+}
