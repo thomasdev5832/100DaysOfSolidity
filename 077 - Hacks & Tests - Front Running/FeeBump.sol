8431ba29def32295290476da0076268ec3831ef4
Gabriel Thome <eugabrielthome@gmail.com>
2023-11-19 16:35:08 -0300

    #077 - Hacks & Tests: Front Running

---
+++ 077 - Hacks & Tests - Front Running/FeeBump.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+// Front Running: malicious actor exploiting advanced knowledge about pending transactions to gain an unfair advantage. 
+// -< involves observing transactions that are about to be included in the blockchain, and then quickly executing a transaction that benefits from the upcoming change in state.
+
+// Mitigating Front Running Risks:
+// Fee Bumping: allow users to increase their transaction's gas fees after submission.
+
+contract FeeBump {
+
+    mapping(address => uint256) public pendingTransactions;
+
+    function submitTransaction(uint256 amount, uint256 gasPrice) external {
+        pendingTransactions[msg.sender] = gasPrice;
+        if(gasPrice < pendingTransactions[msg.sender]) {
+            pendingTransactions[msg.sender] = gasPrice;
+        }
+    }
+
+}
