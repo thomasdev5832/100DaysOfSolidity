2bde237170845c034f63ea2df4bc3709cbd112c9
Gabriel Thome <eugabrielthome@gmail.com>
2023-11-30 19:57:28 -0300

    #088 - DeFi: Uniswap V3 Liquidity

---
+++ 088 - DeFi - Uniswap V3 Liquidity/UniswapV3Liquidity.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "./IERC721Receiver.sol";
+import "./INonfungiblePositionManager.sol";
+import "./IERC20.sol";
+import "./IWETH.sol";
+
+/*
+    🚀 Uniswap V3 enables liquidity providers to narrow down their price ranges for added precision. 
+    This means that instead of spreading your assets across a wide price spectrum, you can now define a specific range in which your assets will be utilized. 
+    This innovation amplifies fee earnings and capital efficiency, setting the stage for our exploration. 
+*/
+
+contract UniswapV3Liquidity is IERC721Receiver {
+    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    IERC20 private constant dai = IERC20(DAI);
+    IWETH private constant weth = IWETH(WETH);
+
+    int24 private constant MIN_TICK = -887272;
+    int24 private constant MAX_TICK = -MIN_TICK;
+    int24 private constant TICK_SPACING = 60;
+
+    INonfungiblePositionManager public nonfungiblePositionManager = INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
+
+    function onERC721Received( 
+        address operator, 
+        address from, 
+        uint tokenId, 
+        bytes calldata
+        ) external returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    } 
+
+    function mintNewPosition(
+        uint amount0ToAdd,
+        uint amount1ToAdd
+    ) external returns (uint tokenId, uint128 liquidity, uint amount0, uint amount1) {
+        dai.transferFrom(msg.sender, address(this), amount0ToAdd);
+        weth.transferFrom(msg.sender, address(this), amount1ToAdd);
+
+        dai.approve(address(nonfungiblePositionManager), amount0ToAdd);
+        weth.approve(address(nonfungiblePositionManager), amount1ToAdd);
+
+        INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
+            token0: DAI,
+            token1: WETH,
+            fee: 3000,
+            tickLower: (MIN_TICK / TICK_SPACING) * TICK_SPACING,
+            tickUpper: (MAX_TICK / TICK_SPACING) * TICK_SPACING,
+            amount0Desired: amount0ToAdd,
+            amount1Desired: amount1ToAdd,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+
+        (tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager.mint(params);
+
+        if(amount0 < amount0ToAdd) {
+            dai.approve(address(nonfungiblePositionManager), 0);
+            uint refund0 = amount0ToAdd - amount0;
+            dai.transfer(msg.sender, refund0);
+        }
+        if(amount1 < amount1ToAdd) {
+            weth.approve(address(nonfungiblePositionManager), 0);
+            uint refund1 = amount1ToAdd - amount1;
+            weth.transfer(msg.sender, refund1);
+        }
+    }
+
+    function collectAllFees(
+        uint tokenId
+    ) external returns (uint amount0, uint amount1) {
+        INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager.CollectParams({
+            tokenId: tokenId,
+            recipient: address(this),
+            amount0Max: type(uint128).max,
+            amount1Max: type(uint128).max
+        });
+        (amount0, amount1) = nonfungiblePositionManager.collet(params);
+    }
+
+    function increaseLiquidityCurrentRange(
+        uint tokenId,
+        uint amount0ToAdd,
+        uint amount1ToAdd
+    ) external returns (uint128 liquidity, uint amount0, uint amount1) {
+        dai.transferFrom(msg.sender, address(this), amount0ToAdd);
+        weth.transferFrom(msg.sender, address(this), amount1ToAdd);
+
+        dai.approve(address(nonfungiblePositionManager), amount0ToAdd);
+        weth.approve(address(nonfungiblePositionManager), amount1ToAdd);
+        
+        INonfungiblePositionManager.IncreaseLiquidityParams memory params = INonfungiblePositionManager.IncreaseLiquidityParams({
+            tokenId: tokenId,
+            amount0Desired: amount0ToAdd,
+            amount1Desired: amount1ToAdd,
+            amount0Min: 0,
+            amount1Min: 0,
+            deadline: block.timestamp
+        });
+
+        (liquidity, amount0, amount1) = nonfungiblePositionManager.increaseLiquidity(params);
+    }
+
+    function decreaseLiquidityCurrentRange(
+        uint tokenId,
+        uint128 liquidity
+    ) external returns (uint amount0, uint amount1) {
+        INonfungiblePositionManager.DecreaseLiquidityParams memory params = INonfungiblePositionManager.DecreaseLiquidityParams({
+            tokenId: tokenId,
+            liquidity: liquidity,
+            amount0Min: 0,
+            amount1Min: 0,
+            deadline: block.timestamp
+        });
+
+        (amount0, amount1) = nonfungiblePositionManager.decreaseLiquidity(params);
+    }
+
+}
