4a5ccb7106540d216cbab393b3544eb294ed8db7
Gabriel Thome <eugabrielthome@gmail.com>
2023-12-02 19:56:09 -0300

    #090 -  DeFi: Uniswap V3 Flash Swap Arbitrage

---
+++ 090 - DeFi - Uniswap V3 Flash Swap Arbitrage/IUniswapV3Pool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+interface IUniswapV3Pool {
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external returns (int amount0, int amount1);
+}
